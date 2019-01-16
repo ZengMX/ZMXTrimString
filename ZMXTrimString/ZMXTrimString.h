f1b641db9c081a8215bcd355313de7438fd6eaa3
Zeng MX <15217687097@163.com>
2019-01-16 21:13:27 +0800

    no message

---
+++ ZMXTrimString/ZMXTrimString.h
@@ -0,0 +1,13 @@
+//
+//  ZMXTrimString.h
+//  ZMXTrimString
+//
+//  Created by zmx on 2019/1/16.
+//  Copyright © 2019年 zmx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZMXTrimString : NSObject
+
+@end
---
+++ ZMXTrimString/ZMXTrimString.m
@@ -0,0 +1,13 @@
+//
+//  ZMXTrimString.m
+//  ZMXTrimString
+//
+//  Created by zmx on 2019/1/16.
+//  Copyright © 2019年 zmx. All rights reserved.
+//
+
+#import "ZMXTrimString.h"
+
+@implementation ZMXTrimString
+
+@end
